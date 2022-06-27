415fa1dd7a250bb38f575d3ce6a558c695db00ce
Ming Cheuk <ming.cheuk@spark64.com>
2022-06-27 16:09:02 +1200

    feat: Example and microphone input

--- basic_pitch_demo/app.py
+++ basic_pitch_demo/app.py
@@ -1,16 +1,36 @@
 import gradio as gr
 from transcribe import Instrument, transcribe
 
-demo = gr.Interface(
-    title="Automatic Music Transcription",
-    description="AMT based on Spotify's Basic Pitch.",
+title = "Automatic Music Transcription"
+description = "AMT based on Spotify's Basic Pitch."
+
+outputs = [gr.Audio(label="Synthesized"), gr.File(label="MIDI File")]
+instrument_dropdown = gr.Dropdown(
+    label="Instrument", choices=[i.value for i in Instrument]
+)
+
+file_demo = gr.Interface(
+    title=title,
+    description=description,
+    allow_flagging="never",
+    fn=transcribe,
+    examples=[["./notebooks/demo_data/nokia.mp3", "8-bit"]],
+    inputs=[gr.Audio(label="Original Audio", type="filepath"), instrument_dropdown,],
+    outputs=outputs,
+)
+
+recording_demo = gr.Interface(
+    title=title,
+    description=description,
     allow_flagging="never",
     fn=transcribe,
     inputs=[
-        gr.Audio(label="Original Audio", type="filepath"),
-        gr.Dropdown(label="Instrument", choices=[i.value for i in Instrument]),
+        gr.Audio(label="Recording", source="microphone", type="filepath"),
+        instrument_dropdown,
     ],
-    outputs=[gr.Audio(label="Synthesized"), gr.File(label="MIDI File")],
+    outputs=outputs,
 )
 
-demo.launch(server_name='0.0.0.0')
+tabs = gr.TabbedInterface([file_demo, recording_demo], ["File Input", "Mic Input"],)
+
+tabs.launch(server_name="0.0.0.0",)
